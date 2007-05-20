4e6cba335cadfeeb7cf5074f22b7cd1e1893c431
pluskid.zju <pluskid.zju@4651143c-f42f-0410-a7dc-93bad6e629e8>
2007-05-20 14:35:41 +0000

    smart-skeleton, wonderful!

--- trunk/50smart-snippet.el
+++ trunk/50smart-snippet.el
@@ -14,7 +14,27 @@
 ;; those non-word snippet can't be triggered by abbrev expand, we
 ;; need to bind them explicitly to some key
 (smart-snippet-with-abbrev-table 'c++-mode-abbrev-table
-  ("{" "{$>\n$>$.\n}$>" 'bol?))
+  ("{" "{$>\n$>$.\n}$>" 'bol?)
+  ;; if not in comment or other stuff(see `c-in-literal'), then
+  ;; inser a pair of quote. if already in string, insert `\"'
+  ("\"" "\"$.\"" '(not (c-in-literal)))	
+  ("\"" "\\\"$." '(eq (c-in-literal) 'string))
+  ;; insert a pair of parenthesis, useful everywhere
+  ("(" "($.)" t)
+  ;; insert a pair of angular bracket if we are writing templates
+  ("<" "<$.>" '(and (not (c-in-literal))
+			 (looking-back "template[[:blank:]]*")))
+  ;; a pair of square bracket, also useful everywhere
+  ("[" "[$.]" t)
+  ;; a pair of single quote, if not in literal
+  ("'" "'$.'" '(not (c-in-literal)))
+  )
 
 (smart-snippet-with-keymap c++-mode-map 'c++-mode-abbrev-table
-  ("{" "{"))
+  ("{" "{")
+  ("\"" "\"")
+  ("(" "(")
+  ("<" "<")
+  ("[" "[")
+  ("'" "'")
+  )
